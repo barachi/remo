e4888ae201cd3c3022c98efd69cbec3c966f852d
yuya chiba <bachi@yuya-no-MacBook-Pro.local>
2016-08-31 00:50:44 +0900

    エラーの微修正

--- main.hs
+++ main.hs
@@ -1,39 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.Wai (responseLBS, Application, rawPathInfo)
+import Network.Wai (responseLBS, Application, rawPathInfo, Response)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200, status404)
 import Database.Redis
 import Control.Monad.IO.Class
+import Data.ByteString.Lazy.Char8 (pack)
+import Data.ByteString.Internal
 
 index = responseLBS
     status200
     [("Content-Type", "text/plain")]
     "hello world"
 
-getStatus test = responseLBS
+getStatus :: String -> Response
+getStatus s = responseLBS
     status200
     [("Content-Type", "text/plain")]
-    "get!"   
+    (pack s)
 
+--connectRedis :: IO [Maybe Data.ByteString.Internal.ByteString]
+connectRedis :: IO (Either Reply (Maybe ByteString))
 connectRedis = do
     conn <- connect defaultConnectInfo
     runRedis conn $ do
         set "a" "apple"
         a <- get "a" 
-        liftIO a
+        return a
 
 notFound = responseLBS
     status404
     [("Content-Type", "text/plain")]
     "not found"
-
 app :: Application
 app request respond =
     respond $ case rawPathInfo request of
     "/" -> index
-    "/get/" -> getStatus =<< connectRedis
+--    "/get/" -> getStatus =<< connectRedis
+    "/get/" -> hoge
     _ -> notFound
+
+
+hoge :: Response
+hoge = do
+    a <- connectRedis
+    responseLBS status200 [] $
+        case a of
+            Right Just s -> s
+            _ -> "error"
 
 main :: IO ()
 main = do
